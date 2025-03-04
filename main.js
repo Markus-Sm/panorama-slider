765a18ab3fd719945f5c759197b5616cb9d3f72b
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-03-04 17:41:25 +0100

    three images on full screen

--- main.js
+++ main.js
@@ -1,65 +1,49 @@
 let xPos = 0;
 
 gsap.timeline()
-    .set('.ring', { rotationY: 180, cursor: 'grab' }) // set initial rotationY so the parallax jump happens off-screen
-    .set('.img', { // apply transform rotations to each image
-        rotateY: (i) => i * -60,
-        transformOrigin: '50% 50% 800px',
-        z: -800,
-        backgroundImage: (i) => 'url(https://picsum.photos/id/' + (i + 32) + '/800/600/)',
-        backgroundPosition: (i) => getBgPos(i),
-        backfaceVisibility: 'hidden'
+    .set(dragger, { opacity: 0 })
+    .set(ring, { rotationY: 180 })
+    .set('.img', {
+      rotateY: (i) => i * -45,
+      transformOrigin: '50% 50% 2200px',
+      z: -2200,
+      backgroundImage: (i) => 'url(https://picsum.photos/id/' + (i + 32) + '/1680/1120/)',
+      backgroundPosition: (i) => getBgPos(i),
+      backfaceVisibility: 'hidden'
     })
     .from('.img', {
-        duration: 1.5,
-        y: 200,
-        opacity: 0,
-        stagger: 0.1,
-        ease: 'expo'
-    })
-    .add(() => {
-        document.querySelectorAll('.img').forEach(img => {
-            img.addEventListener('mouseenter', (e) => {
-                let current = e.currentTarget;
-                gsap.to('.img', { opacity: (i, t) => (t == current) ? 1 : 0.5, ease: 'power3' })
-            });
-
-            img.addEventListener('mouseleave', (e) => {
-                gsap.to('.img', { opacity: 1, ease: 'power2.inOut' })
-            });
-        });
-    }, '-=0.5');
-
-document.addEventListener('mousedown', dragStart);
-document.addEventListener('touchstart', dragStart);
-
-function dragStart(e) {
-    if (e.touches) e.clientX = e.touches[0].clientX;
-    xPos = Math.round(e.clientX);
-    gsap.set('.ring', { cursor: 'grabbing' });
-    document.addEventListener('mousemove', drag);
-    document.addEventListener('touchmove', drag);
-}
-
-function drag(e) {
-    if (e.touches) e.clientX = e.touches[0].clientX;
-    gsap.to('.ring', {
-        rotationY: '-=' + ((Math.round(e.clientX) - xPos) % 360),
-        onUpdate: () => { gsap.set('.img', { backgroundPosition: (i) => getBgPos(i) }) }
+      duration: 1.5,
+      y: 200,
+      opacity: 0,
+      stagger: 0.1,
+      ease: 'expo'
     });
 
+Draggable.create(dragger, {
+  type: 'x',
+  inertia: true,
+  
+  onDragStart: (e) => {
+    if (e.touches) e.clientX = e.touches[0].clientX;
     xPos = Math.round(e.clientX);
-}
+  },
+  
+  onDrag: (e) => {
+    if (e.touches) e.clientX = e.touches[0].clientX;
+    
+    gsap.to(ring, {
+      rotationY: '-=' + ((Math.round(e.clientX) - xPos) % 360),
+      onUpdate: () => { gsap.set('.img', { backgroundPosition: (i) => getBgPos(i) }) }
+    });
+    
+    xPos = Math.round(e.clientX);
+  },
+  
+  onDragEnd: () => {
+    gsap.set(dragger, { x: 0, y: 0 });
+  }
+});
 
-document.addEventListener('mouseup', dragEnd);
-document.addEventListener('touchend', dragEnd);
-
-function dragEnd(e) {
-    document.removeEventListener('mousemove', drag);
-    document.removeEventListener('touchmove', drag);
-    gsap.set('.ring', { cursor: 'grab' });
-}
-
-function getBgPos(i) { //returns the background-position string to create parallax movement in each image
-    return (100 - gsap.utils.wrap(0, 360, gsap.getProperty('.ring', 'rotationY') - 180 - i * 60) / 360 * 500) + 'px 0px';
+function getBgPos(i) {
+  return (-gsap.utils.wrap(0, 360, gsap.getProperty(ring, 'rotationY') - 180 - i * 45) / 360 * 1680) + 'px 0px';
 }
